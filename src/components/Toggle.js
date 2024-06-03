83d2923324acb663495d5020570a3294a538ca09
Taylor Ng <taylormng95@gmail.com>
2024-06-03 19:25:38 -0400

    completes code along lab

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  let [status, changeStatus] = useState(true)
+
+  function handleClick() {
+    /* changeStatus((status) => !status)
+    changeStatus((status) => !status) */
+    changeStatus((status) => !status)
+    changeStatus((status) => !status)
+  }
+
+  console.log(status)
+
+  const color = status ? "red" : "white"
+
+  return <button style={{color: color}} onClick={handleClick}>{status ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
